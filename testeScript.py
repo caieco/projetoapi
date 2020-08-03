95acc6085c0a63991dc419bdfa76ffd35c25e04d
JulieneCaroline <julienecaroline79@gmail.com>
2020-08-03 20:09:13 -0300

    Puxando dados da binance

--- testeScript.py
+++ testeScript.py
@@ -1,55 +1,38 @@
 # -*- coding: utf-8 -*-
+
+#ASK = Compra (verde)
+#BID = Venda (vermelho)
 """
 Editor Spyder
 
 Este é um arquivo de script temporário.
 """
-
-from binance_f import RequestClient
-from binance_f.constant.test import *
-from binance_f.base.printobject import *
-import pandas as pd
+import requests
 import json
-from binance.client import Client
-import time
-      
 
 
+baseBinance = 'https://api.binance.com'
+endpointBinance = '/api/v3/depth'
+payload = {'symbol': 'BTCUSDT', 'limit': '5'}
 
-g_api_key = ''
-g_secret_key = ''
-client = Client(g_api_key, g_secret_key)
-info = client.get_symbol_info('BTCUSDT')
-while True:
-    print("Executando")
-    depth = client.get_order_book(symbol='BTCUSDT', limit=10)
-    asks = pd.DataFrame(depth['asks'], columns=['C Preço do Bitcoin', 'C Quantidade de Bitcoin'])
-    asks.insert(2, 'C Valor da Ordem', None)
-    for i in asks.index:
-        asks.loc[i, 'C Valor da Ordem'] = float(asks.loc[i, 'C Preço do Bitcoin']) * float(asks.loc[i, 'C Quantidade de Bitcoin'])
+r = requests.get(baseBinance + endpointBinance, params=payload)
+resposta1 = json.loads(r.text)
+volumeBTCBids = 0
+for bid in resposta1['bids']:
+    volumeBTCBids += float(bid[1])
     
-    bids = pd.DataFrame(depth['bids'], columns=['V Preço do Bitcoin', 'V Quantidade de Bitcoin'])
-    asks.insert(3, 'VENDA', ' ')
-    asks.insert(4, 'V Preço do Bitcoin', bids.loc[:, 'V Preço do Bitcoin'])
-    asks.insert(5, 'V Quantidade de Bitcoin', bids.loc[:, 'V Quantidade de Bitcoin'])
-    for i in bids.index:
-        asks.loc[i, 'V Valor da Ordem'] = float(bids.loc[i, 'V Preço do Bitcoin']) * float(bids.loc[i, 'V Quantidade de Bitcoin'])
-    asks.insert(0, 'COMPRA', ' ')
-    asks.update(asks)
-    for i in asks.index:
-        print(asks.loc[i, :])
-        trades = client.get_recent_trades(symbol='BNBBTC')
-        
-    
-        
-#request_client = RequestClient(api_key=g_api_key, secret_key=g_secret_key)
+volumeBTCAsks = 0
+for ask in resposta1['asks']:
+    volumeBTCAsks += float(ask[1])
+# lastBid = resposta1['bids'][4]
+# lastAsk = resposta1['asks'][4]
 
-#result = request_client.get_ticker_price_change_statistics()
-#print(str(result[0]))
-# result = request_client.get_ticker_price_change_statistics(symbol="BTCUSDT")
+endpointBinance = '/api/v3/ticker/24hr'
+payload = {'symbol': 'BTCUSDT'}   
 
-#for ticker in result:
-    #print(ticker.askPrice)
-    #obj = []
-    #'askPrice', 'bidPrice', 'closeTime', 'count', 'firstId', 'highPrice', 'json_parse', 'lastId', 'lastPrice', 'lastQty', 'lowPrice', 'openPrice', 'openTime', 'priceChange', 'priceChangePercent', 'quoteVolume', 'symbol', 'volume', 'weightedAvgPrice'
-    
+r = requests.get(baseBinance + endpointBinance, params=payload)
+resposta = json.loads(r.text)
+volumeDolar = resposta['quoteVolume']
+volumeBTC = resposta['volume']
+
+
